7eb0dc380f7a74312d3048eed8e2cd13bd03fed0
aahmad94 <aka5vm@virginia.edu>
2018-03-21 00:17:39 -0700

    call YouTube API to fetch data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar'; 
 
 const API_KEY = 'AIzaSyDQ1PNjn2W3s-3SFcs8rHFqLlyJA-1ckFQ';
 
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
+
 const App = () => {
-  return <div>Test!</div>;
+  return (
+    <div>
+      <SearchBar />
+  </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.root'));
