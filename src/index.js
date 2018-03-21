6cc3acadd97704828575b534e79fb32a8af73d2c
aahmad94 <aka5vm@virginia.edu>
2018-03-21 00:57:27 -0700

    build videolist component w/ api call data

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar'; 
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDQ1PNjn2W3s-3SFcs8rHFqLlyJA-1ckFQ';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-  </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos: [] };
+    
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.root'));
